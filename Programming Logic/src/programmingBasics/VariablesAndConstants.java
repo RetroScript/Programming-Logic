98cb7b3985a1dc49d861486e203c70a079bd1633
RetroScript <andrefrancelino06@gmail.com>
2023-12-10 06:33:05 -0300

    variables and constants explanation update

--- Programming Logic/src/programmingBasics/VariablesAndConstants.java
+++ Programming Logic/src/programmingBasics/VariablesAndConstants.java
@@ -1,41 +1,41 @@
 package programmingBasics;
 
-public class Variables {
+public class VariablesAndConstants {
 
 
 	//MORE INFO ABOUT LONG:
 			/*
-			 * when giving a value to a long type java will consider this value as an int by default
-			 * which is even shorter than a long number, so in order to put more numbers, is needed
-			 * to write l which indicates that the value is a long one
+			 * when giving a value to a long type java will consider this value as an int 
+			 * by default which is even shorter than a long number, so in order to put more 
+			 * numbers, is needed to write l which indicates that the value is a long one
 			 *
 			 *	
 			 *	long test = 908032813018l;
 			 *
 			 */
 	
-	//VARIABLES:
+	//VALUE TYPES:
 		/*
-		 * variables are a group of values with variable types that can be changed during the 
-		 * execution of the application.
-		 * 
 		 * The value types can be divided into:
 		 * 
 		 * Primitive: int, long, byte, short, double, float, char and boolean.
 		 *	and
 		 * Non-primitive: String, Array, Interface, Object, Class and others **based from objects**
-		 * 
 		 */
 	
-		//ATENTION!!!
-			/*
-			 * for learning reasons this document will adopt a considerable use of methods, as an
-			 * intention of simplifying the divergent parts into the code besides simplifying 
-			 * the explanation and the examples as well. these elements will be explained more 
-			 * deeply as the project advance but, for now, it's clever to consider them as 
-			 * simple chunks of code that organizes the overall parts.
-			 */
+	//VARIABLES:
+		/*
+		 * variables are a group of values with types that can be changed during the 
+		 * execution of the application.
+		 */
 	
+	//CONSTANTS:
+		/*
+		 * constants are pieces of data which never changes during the execution of the 
+		 * application, keeping the same value that has been set by default.
+		 */
+	
+		
 	public static void main(String[] args) {
 	
 
@@ -50,6 +50,7 @@ public class Variables {
 		 * 
 		 */
 	
+		//VARIABLES
 		
 		//Java syntax for VARIABLES
 			/*
@@ -59,14 +60,14 @@ public class Variables {
 			 * 4-semicolon to conclude the sentence
 			 */
 		
-		//EXAMPLES: here are some of the most used
-		int number = 0;
-		double decimalNumber = 0.11;
-		long longNumber = 100000000000l;
-		boolean trueFalse = true;
-		
-		//EXAMPLE: String is non-primitive value type since it have properties
-		String text = "piece of text";
+			//EXAMPLES: here are some of the most used
+			int number = 0;
+			double decimalNumber = 0.11;
+			long longNumber = 100000000000l;
+			boolean trueFalse = true;
+			
+			//EXAMPLE: String is non-primitive value type since it have properties
+			String text = "piece of text";
 		
 		//here are some BEHAVIORS, a common thing of objects, that are defined by methods
 		text.toUpperCase(); //It's a behavior, that make everything uppercase!
@@ -82,17 +83,32 @@ public class Variables {
 			 * 3-write the desired value
 			 */
 		
-		//EXAMPLE:
-		number = 10;
-		decimalNumber = 0.5;
-		longNumber = 99999999999l;
-		trueFalse = false;
+			//EXAMPLE:
+			number = 10;
+			decimalNumber = 0.5;
+			longNumber = 99999999999l;
+			trueFalse = false;
+			
+			text = "Hello World!";
 		
-		text = "Hello World!";
+		
+			
+		
+		//CONSTANTS:
+		
+		//Java syntax for CONSTANTS
+		/*
+		 * the "final" keyword
+		 * do the same process for VARIABLES
+		 */
+	
+			//EXAMPLE: 
+			final double pi = 3.14;
 		
 		
+			
 		
-		//Using the variables:
+		//Using the values:
 		
 		//their value will be show at the console!
 		System.out.println(number);
@@ -100,10 +116,7 @@ public class Variables {
 		System.out.println(longNumber);
 		System.out.println(trueFalse);
 		System.out.println(text);
+		System.out.println(pi);
 	}
-	
-	
-	
-	
 	
 }
